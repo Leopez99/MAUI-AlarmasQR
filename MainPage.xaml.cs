31327d98490ce54dd7704f74ef950e4f3a2105aa
Leo <leonardo@pezoa.com>
2026-07-28 11:53:02 -0300

    Cofig inicial terminada sin clases

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,13 @@
-﻿namespace MAUI_AlarmasQR
+﻿using MAUI_AlarmasQR._ViewModel;
+
+namespace MAUI_AlarmasQR
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            BindingContext = new MainViewModel();
         }
     }
 }
---
+++ _ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAUI_AlarmasQR._ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public CounterButtonViewModel CounterButton { get; } = new();
+    }
+}
