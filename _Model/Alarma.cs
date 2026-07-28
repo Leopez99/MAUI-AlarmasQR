f0e1187d87ef13a1d4c6daa312bf8119b474c64f
Leo <leonardo@pezoa.com>
2026-07-28 12:13:05 -0300

    Creadas clases Alarma y Cliente

---
+++ _Model/Alarma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAUI_AlarmasQR._Model
+{
+    internal class Alarma
+    {
+        public string piso { get; private set; }
+        public string area { get; private set; }
+        public string nombre { get; private set; }
+
+        public Alarma(string piso, string area, string nombre)
+        {
+            this.piso = piso;
+            this.area = area;
+            this.nombre = nombre;
+        }
+    }
+}
---
+++ _Model/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MAUI_AlarmasQR._Model
+{
+    internal class Cliente
+    {
+        public string nombre {  get; private set; }
+        public ObservableCollection<Alarma> ListaDeAlarmas { get; } = new();
+        //public string registroDeMatenimientos { get; private set; } - Esto va a ser una coleccion de los distintos PDF que se fueron exportando de este cliente
+
+        public Cliente(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+    }
+}
