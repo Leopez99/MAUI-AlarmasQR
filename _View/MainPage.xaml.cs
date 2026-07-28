9cf92b2cdf2d958995c5bed4a6dbed6551a69e67
Leo <leonardo@pezoa.com>
2026-07-28 14:40:50 -0300

    Pagina de inicio solo vista

---
+++ _View/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MAUI_AlarmasQR._ViewModel;
+
+namespace MAUI_AlarmasQR;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnCrearAlarmaClicked(object sender, EventArgs e)
+    {
+        // await Navigation.PushAsync(new CrearAlarmaPage());
+    }
+
+    private async void OnComenzarMantenimientoClicked(object sender, EventArgs e)
+    {
+        // await Navigation.PushAsync(new MantenimientoPage());
+    }
+}
